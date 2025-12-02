d601622adb954a4d71a87c3943eb7614b4a5d169
EC2 Default User <ec2-user@ip-172-31-35-163.ap-south-1.compute.internal>
2025-12-02 09:16:16 +0000

    Add script for Jenkins build

---
+++ script1.py
@@ -0,0 +1,18 @@
+# scrip1.py
+
+import time
+
+def main():
+        print("Starting Jenkins build test...")
+
+            # Simulating a failure in the script to test the email notification
+                time.sleep(2)
+                    
+                        # Uncomment the line below to simulate a failure
+                            # raise Exception("This is a simulated failure")
+
+                                print("Jenkins build test completed successfully.")
+
+                                if __name__ == "__main__":
+                                        main()
+
